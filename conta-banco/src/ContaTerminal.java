1ac9de69b5704a5593a37f63aba1c7ca6bdc8edb
Wallace <wallacegoncalves0011@gmail.com>
2024-07-07 18:18:49 -0300

    Finalização

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,40 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Definir o Locale para o programa como americano
+        Locale.setDefault(Locale.US);
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        int numero = teclado.nextInt();
+        teclado.nextLine();
+        System.out.println("Usuário: "+numero+" (depois ENTER para o próximo campo)");
+        teclado.nextLine();
+
+        System.out.print("Por favor, digite a Agência: ");
+        String agencia = teclado.next();
+        teclado.nextLine();
+        System.out.println("Agência: "+agencia+" (depois ENTER para o próximo campo)");
+        teclado.nextLine();
+
+        System.out.print("Por favor, digite o Nome do Cliente: ");
+        String nome = teclado.next().toUpperCase();
+        teclado.nextLine();
+        System.out.println("Nome: "+nome+" (depois ENTER para o próximo campo)");
+        teclado.nextLine();
+
+        System.out.print("Por favor, informe o Saldo: ");
+        double saldo = teclado.nextDouble();
+        teclado.nextLine();
+        System.out.println("Saldo: "+saldo+" (depois ENTER para o próximo campo)");
+        teclado.nextLine();
+
+        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.\n");
+
     }
 }
